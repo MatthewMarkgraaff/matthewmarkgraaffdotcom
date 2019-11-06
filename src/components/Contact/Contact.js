05683888f64f1542d6ff87cf86ed4d5136a4d13c
Matthew Markgraaff <matthewmarkgraaff@gmail.com>
2019-11-06 22:21:07 +0200

    fix

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,23 +1,42 @@
 // @flow strict
 import React, { useState } from 'react';
 import styles from './Contact.module.scss';
+import Banner from '../Banner';
 
 const Contact = () => {
+  const [submitted, setSubmitted] = useState(false)
+  const [contact, setContact] = useState({
+    name: '',
+    email: '',
+    message: ''
+  })
+
+  async function handleSubmit(e) {
+    setSubmitted(true)
+  }
+
   return (
     <React.Fragment>
-      <form method="post" className={styles['contact']} netlify-honeypot="bot-field" data-netlify="true" name="contact">
+      {
+        submitted && (
+          <Banner bannerType="success">
+            Success! Thank you for reaching out. I'll be in touch soon.
+          </Banner>
+        )
+      }
+      <form onSubmit={handleSubmit} method="post" className={styles['contact']} netlify-honeypot="bot-field" data-netlify="true" name="contact">
         <input type="hidden" name="bot-field" />
         <label>
           Name
-          <input type="text" name="name" id="name" />
+          <input type="text" name="name" id="name" value={contact.name} onChange={(e)=>setContact({ ...contact, name: e.target.value })} />
         </label>
         <label>
           Email
-          <input type="email" name="email" id="email" />
+          <input type="email" name="email" id="email" value={contact.email} onChange={(e)=>setContact({ ...contact, email: e.target.value })} />
         </label>
         <label>
           Message
-          <textarea name="message" id="message" rows="5" />
+          <textarea name="message" id="message" rows="5" value={contact.message} onChange={(e)=>setContact({...contact, message: e.target.value })} />
         </label>
         <button type="submit">Submit</button>
       </form>
